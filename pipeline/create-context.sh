e640a1082eef5040fc3b633f48d91dc4677448fd
ldangelo@mac.com <ldangelo@mac.com>
2018-06-26 08:52:26 -0400

    More debugging information

--- pipeline/create-context.sh
+++ pipeline/create-context.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# let's see what's in our parent directories?
+ls -R ..
+
 mkdir /tmp/app
 
+# copy the artifacts to the app directory
 cp -R ../_build/dev/rel/socialshare /tmp/app/socialshare 
